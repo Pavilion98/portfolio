4812e5a634506dd2eecd2c0ac6436721bcebde13
wassim <wassim.alharakeh@mubs.edu.lb>
2021-02-06 20:25:09 +0200

    added some margin fixes

--- index.js
+++ index.js
@@ -2,19 +2,14 @@ var box = document.querySelector(".main-menu");
 
 document.querySelector('.menu-btn').addEventListener('click', () => {
     box.classList.toggle('show');
-
-    // if (event.target != box && event.target.parentNode != box) {
-    //     box.style.display = "none";
-    // }
+   
 });
 
 // window.addEventListener('mouseup', function(event) {
 //     const box = document.querySelector(".main-menu");
 //     if (event.target != box && event.target.parentNode != box) {
 //         box.style.display = "none";
-//     } else {
-//         box.classList.toggle('show');
-//     }
+//     } 
 // });
 
 
@@ -25,9 +20,9 @@ document.querySelector('.menu-btn').addEventListener('click', () => {
 //Typed js typing effect
 $('document').ready(function(){
     var typed = new Typed('#typed',{
-        strings: ["Hi,^1000I'm Wassim Harakeh", "I'm A junior Web Developer"],
+        strings: ["Hi,^1000I'm Wassim Harakeh"],
         typeSpeed: 80,
-        loop: true
+        loop: false
     });
 });
 
@@ -52,7 +47,6 @@ $('.navbar a').on('click', function(e){
 
 
 
-//Anime js
 
 
 
