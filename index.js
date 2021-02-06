7cd640ac63e5c5282de71a4b9e6743ee2f406287
wassim <wassim.alharakeh@mubs.edu.lb>
2021-02-06 04:28:20 +0200

    updated typed js strings

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
-document.querySelector('.menu-btn').addEventListener('click', () => document.querySelector('.main-menu').classList.toggle('show'));
+var box = document.querySelector(".main-menu");
+
+document.querySelector('.menu-btn').addEventListener('click', () => {
+    box.classList.toggle('show');
+
+    // if (event.target != box && event.target.parentNode != box) {
+    //     box.style.display = "none";
+    // }
+});
+
+// window.addEventListener('mouseup', function(event) {
+//     const box = document.querySelector(".main-menu");
+//     if (event.target != box && event.target.parentNode != box) {
+//         box.style.display = "none";
+//     } else {
+//         box.classList.toggle('show');
+//     }
+// });
+
 
 
 
@@ -7,8 +25,9 @@ document.querySelector('.menu-btn').addEventListener('click', () => document.que
 //Typed js typing effect
 $('document').ready(function(){
     var typed = new Typed('#typed',{
-        strings: ["Hi,^1000I'm Wassim Harakeh"],
-        typeSpeed: 40
+        strings: ["Hi,^1000I'm Wassim Harakeh", "I'm A junior Web Developer"],
+        typeSpeed: 80,
+        loop: true
     });
 });
 
